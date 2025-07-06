ba17045fe9f45d54224ad9a55eb406d90c7f71c9
zygomaticontheway <mrzygomatic@gmail.com>
2025-07-07 00:45:34 +0200

    codilyty directory

---
+++ codility/src/main.java
@@ -0,0 +1,19 @@
+import com.sun.jdi.Value;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class main {
+    public static void main(String[] args) {
+
+        int[] array = {-123, 10, 12, 1, 2, 3, 4, 5, 5, 6, 6, 7, 2, 2, 2, 2, 2, 2, 2, 2};
+        int[] array1 = {1, 2, 3, 4, 5, 6, 7, 2};
+        int[] pairedArray = {2, 2, 8, 8, 5, 6, 6, 7, 5, 7, 8, 6};
+        int[] missingNrArray = {2, 4, 3, 6, 1, 0};
+        int[] progression = {12, 4, 6, 8, 10, 2};
+        int[] cars = {0, 1, 1, 1, 0, 0, 1};
+
+//        System.out.println("dominator " + dominator(array1));
+
+    }
+}
